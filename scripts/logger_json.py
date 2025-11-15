3e929384fdf4d82fb63041e4248e74a1c5590081
EdwinEstrada102 <87724218+EdwinEstrada102@users.noreply.github.com>
2025-11-14 16:00:46 -0800

    Add files via upload

---
+++ scripts/logger_json.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import time
+import json
+import requests
+
+# Kraken DOA endpoint (LOCALHOST on Pi)
+DOA_URL = "http://127.0.0.1:8081/DOA_value.html"
+
+# Output JSON log file (JSON Lines format)
+OUT_FILE = "/home/ngcp25/kraken_logs/doa_log.jsonl"
+
+# Polling rate (seconds)
+UPDATE_RATE = 0.1  # 10 Hz or match Kraken update rate
+
+count=-1
+def log_once():
+    global count
+    
+    try:
+        r = requests.get(DOA_URL, timeout=1)
+        line = r.text.strip()
+        fields = line.split(',')
+        # Build JSON object with selected fields
+        entry = {
+            "epoch": float(fields[0]),
+            "doa": float(fields[1]),               # unit-circle DoA (0°=East CCW)
+            "confidence": float(fields[2]),
+            "lat": float(fields[9]),
+            "lon": float(fields[10]),
+            "gps_heading": float(fields[11]),
+            #"compass_heading": float(fields[12]),
+        }
+
+        # Append JSON object to file
+        with open(OUT_FILE, "a") as f:
+            if count != float(fields[0]):
+                print(count)
+                print(float(fields[0]))
+                f.write(json.dumps(entry) + "\n")
+                count=float(fields[0])
+
+    except Exception as e:
+        print("Error:", e)
+    
+
+def main():
+    while True:
+        log_once()
+        time.sleep(UPDATE_RATE)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
